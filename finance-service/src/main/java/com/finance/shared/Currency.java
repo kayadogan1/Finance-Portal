6c7a2e546a1598a395f89e6a08b7556595342c69
dogankaya <dogankaya2555@gmail.com>
2026-02-09 02:27:59 +0300

    Add GitHub Actions deployment and docker file workflow

--- finance-service/src/main/java/com/finance/shared/Currency.java
+++ finance-service/src/main/java/com/finance/shared/Currency.java
@@ -1,12 +1,31 @@
 package com.finance.shared;
 
 public enum Currency {
-    TRY,
-    USD,
-    EUR,
-    GBP,
-    JPY,
-    USDT,
-    BTC,
-    ETH
+    // Fiat Para Birimleri
+    AUD,  // Australian Dollar
+    CAD,  // Canadian Dollar
+    CHF,  // Swiss Franc
+    CNY,  // Chinese Yuan
+    DKK,  // Danish Krone
+    EUR,  // Euro
+    GBP,  // British Pound
+    INR,  // Indian Rupee
+    JPY,  // Japanese Yen
+    NOK,  // Norwegian Krone
+    NZD,  // New Zealand Dollar
+    RUB,  // Russian Ruble
+    SAR,  // Saudi Riyal
+    SEK,  // Swedish Krona
+    TRY,  // Turkish Lira
+    USD,  // US Dollar
+    ZAR,  // South African Rand
+
+    // Kripto Para Birimleri
+    BTC,  // Bitcoin
+    ETH,  // Ethereum
+    USDT, // Tether
+
+    // Emtia Birimleri (Gold/Silver için)
+    XAU,  // Gold (Troy Ounce)
+    XAG   // Silver (Troy Ounce)
 }
