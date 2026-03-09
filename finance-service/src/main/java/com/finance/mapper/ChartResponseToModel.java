56773f39e4ac33a100f83a6e3105fb0e3fa9dc9e
dogankaya <dogankaya2555@gmail.com>
2026-03-09 14:37:31 +0300

    [Skip Ci] fix yahoo timestamp issues with using Instant utc

--- finance-service/src/main/java/com/finance/mapper/ChartResponseToModel.java
+++ finance-service/src/main/java/com/finance/mapper/ChartResponseToModel.java
@@ -9,28 +9,39 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChartResponseToModel {
 
     public static List<MarketData> toMarketDataList(YahooChartResponse yahooChartResponse, Instrument instrument) {
         var result = yahooChartResponse.chart().result().getFirst();
+
         List<Long> timestamps = result.timestamp();
+        if (timestamps == null || timestamps.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         List<BigDecimal> closes = result.indicators().quote().getFirst().close();
+        if (closes == null || closes.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         List<MarketData> marketDataList = new ArrayList<>();
-        for (int i=0; i<timestamps.size(); i++) {
+        for (int i = 0; i < timestamps.size(); i++) {
+            BigDecimal price = closes.get(i);
+            if (price == null) continue;
 
             LocalDateTime dateTime = Instant.ofEpochSecond(timestamps.get(i))
                     .atZone(ZoneId.systemDefault()).toLocalDateTime();
-            MarketData marketData = MarketData.builder()
-                    .price(closes.get(i))
+
+            marketDataList.add(MarketData.builder()
+                    .price(price)
                     .timestamp(dateTime)
                     .instrument(instrument)
-                    .build();
-            marketDataList.add(marketData);
+                    .build());
         }
         return marketDataList;
-
     }
 
 }
